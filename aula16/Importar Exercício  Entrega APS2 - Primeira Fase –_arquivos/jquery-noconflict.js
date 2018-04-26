a83a9a1c390aeaf9dd5d687448bd18b7b63ca148
Pedro Cunial <pedrocc4@al.insper.edu.br>
2018-04-26 13:32:35 -0300

    Quick commit - Thu Apr 26 13:32:35 -03 2018

--- aula16/Importar Exercício  Entrega APS2 - Primeira Fase –_arquivos/jquery-noconflict.js
+++
@@ -1,8 +0,0 @@
-
-// Note: include this file when using prototype and jQuery on the same page. Be sure to include prototype.js 
-// and jquery.js before this file.
-// The jQuery $ function will be remapped to $j as to not conflict with the prototype version of that function.
-
-// Be aware that some jQuery plugins may use the $ function and in that case, they should be edited to use $j instead.
-
-document.write("<script type=\"text/javascript\">jQuery.noConflict();var $j = jQuery;</script>");
